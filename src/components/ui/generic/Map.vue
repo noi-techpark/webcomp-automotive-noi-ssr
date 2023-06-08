f84d4391b84a975940e97f1b7ff15c06974a9857
RaphaelSiller <raphael.siller123@gmail.com>
2023-06-08 10:11:38 +0200

    feat: reworked Map and added clusterization

--- src/components/ui/generic/Map.vue
+++ src/components/ui/generic/Map.vue
@@ -25,33 +25,11 @@ SPDX-License-Identifier: AGPL-3.0-or-later
       <vl-source-osm></vl-source-osm>
     </vl-layer-tile>
 
-    <vl-layer-vector
-      v-for="(company, index) in companiesWithValidLocationCoordinates"
-      :key="index"
-    >
-      <vl-feature :id="getSecureMapFeatureId(company.id)">
-        <vl-geom-point
-          :coordinates="[company.coordinates.lng, company.coordinates.lat]"
-        ></vl-geom-point>
-
-        <vl-style-box>
-          <vl-overlay
-            v-if="currentZoom >= 14"
-            :id="getSecureMapFeatureId(company.id) + 'overlay'"
-            :position="[company.coordinates.lng, company.coordinates.lat]"
-          >
-            <div class="alert" @click="clickedBadge(company.id)">
-              {{ company.name }}
-            </div>
-          </vl-overlay>
-          <vl-style-icon
-            src="https://cdn.webcomponents.opendatahub.testingmachine.eu/dist/e3df9ad8-e78f-48d8-88d2-089657d27de5/marker.png"
-            :data-webpack-sync="require('@/assets/image/marker.png')"
-            :anchor="[0.5, 1]"
-            :scale="0.1"
-          ></vl-style-icon>
-        </vl-style-box>
-      </vl-feature>
+      <vl-layer-vector>
+        <vl-source-cluster :distance="20">
+          <vl-source-vector :features="points"></vl-source-vector>
+        </vl-source-cluster>
+        <vl-style-func :factory="markerStyleFunc" />
     </vl-layer-vector>
   </vl-map>
 </template>
@@ -60,6 +38,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 import Vue from 'vue'
 import VueLayers from 'vuelayers'
 import 'vuelayers/lib/style.css'
+
+import Style from 'ol/style/Style'
+import OlIcon from 'ol/style/Icon'
+import Fill from 'ol/style/Fill'
+import Stroke from 'ol/style/Stroke'
+import OlText from 'ol/style/Text'
 
 Vue.use(VueLayers)
 
@@ -96,6 +80,21 @@ export default {
         this.hasCompanyValidCoordinates(company)
       )
     },
+    points() {
+      return this.getCoordinatesOfCompanies(this.companiesWithValidLocationCoordinates);
+    },
+    mapMarker() {
+      return new OlIcon({
+        src: "https://cdn.webcomponents.opendatahub.testingmachine.eu/dist/e3df9ad8-e78f-48d8-88d2-089657d27de5/marker.png",
+        scale: 0.1,
+      });
+    },
+    clusterIcon() {
+      return new OlIcon({
+        src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg", // https://it.m.wikipedia.org/wiki/File:RedDot.svg
+        scale: 1.5,
+      });
+    }
   },
 
   methods: {
@@ -116,6 +115,14 @@ export default {
       return Number(mapFeatureId.split('-')[0])
     },
 
+    getCompany(companyID) {
+      return this.companiesWithValidLocationCoordinates.find(company => company.id === companyID);
+    },
+
+    getCompanyFromMapFeatureID(mapFeatureId) {
+      return this.getCompany(this.getCompanyIdFromMapFeatureId(mapFeatureId));
+    },
+
     handleMapMove() {
       this.currentZoom = this.$refs.map.getView().getZoom()
     },
@@ -131,20 +138,79 @@ export default {
       if (!feature) {
         return
       }
-
-      this.clickedMarker(feature.id_)
-    },
-
-    clickedBadge(companyId) {
-      this.$emit('showCompanyWithId', companyId)
+      if (feature?.values_?.features.length === 1)
+        // feature.values_?.features[0]?.id_ = MapFeatureId
+        this.clickedMarker(feature.values_?.features[0]?.id_)
+      else {
+        // Zoom to the cluster
+        this.$refs.map.getView().animate({
+          zoom: this.$refs.map.getView().getZoom() + 2,
+          duration: 250,
+          anchor: feature?.values_?.geometry?.flatCoordinates,
+        })
+      }
     },
 
     clickedMarker(mapFeatureId) {
-      this.$emit(
-        'showCompanyWithId',
-        this.getCompanyIdFromMapFeatureId(mapFeatureId)
-      )
+        this.$emit(
+          'showCompanyWithId',
+          this.getCompanyIdFromMapFeatureId(mapFeatureId)
+        )
     },
+
+    /**
+     * @param companies: Array of companies, for example companiesWithValidLocationCoordinates
+     * 
+     * @returns Array in GeoJSON Format
+     */
+    getCoordinatesOfCompanies(companies) {
+      const ret = [];
+      for (const company of companies) {
+        const {
+          lat,
+          lng,
+          type = 'Feature',
+          id = this.getSecureMapFeatureId(company.id),
+          geometry = { type: 'Point', coordinates: [lng, lat] },
+        } = company.coordinates;
+        ret.push({ lat, lng, type, id, geometry });
+      }
+      return ret;
+    },
+
+    textFormatForMarkerStyleFunc(mapFeatureId) {
+      let ret = this.getCompanyFromMapFeatureID(mapFeatureId)?.name;
+      if (ret.length > 20) {
+        ret = ret.substring(0, 17) + "...";
+      }
+      return ret
+    },
+
+    markerStyleFunc() {
+      // style function and styles using OpenLayers API
+      // https://openlayers.org/en/latest/apidoc/module-ol_style_Style.html
+      return (feature) => {
+        this.curFeatureIndex++
+        const baseStyle = new Style({
+          image: feature?.values_?.features.length <= 1 ? this.mapMarker : this.clusterIcon,
+          text: feature?.values_?.features.length <= 1 ? new OlText({
+            text: this.currentZoom >= 14 ? this.textFormatForMarkerStyleFunc(feature?.values_?.features[0]?.id_) : undefined,
+            fill: new Fill({ color: '#e5e7eb' }),
+            backgroundFill: new Fill({ color: '#c70000' }),
+            textAlign: 'center',
+            offsetY: -25,
+            scale: 1.2,
+            backgroundStroke: new Stroke({
+              color: '#c70000',
+              width: 5,
+            }),
+            padding: [1.25, 1.75, 1.25, 1.75]
+          }) : undefined,
+          zIndex: this.curFeatureIndex,
+        })
+        return [baseStyle];
+      };
+    }
   },
 }
 </script>
@@ -165,7 +231,7 @@ export default {
     margin-left: -50px;
   }
 
-  & .ol-zoom {
+  & .vl-zoom {
     top: auto !important;
     left: auto !important;
     bottom: 30px;
@@ -173,7 +239,7 @@ export default {
   }
 
   &.is-preview {
-    & .ol-zoom {
+    & .vl-zoom {
       display: none !important;
     }
   }
