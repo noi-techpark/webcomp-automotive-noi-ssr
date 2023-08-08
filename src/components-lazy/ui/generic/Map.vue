fb4dc74aacea185e663ea3c5f5c1d9da4491ee1f
RaphaelSiller <raphael.siller123@gmail.com>
2023-08-08 09:51:40 +0200

    fix: map didn't show any markers

--- src/components-lazy/ui/generic/Map.vue
+++ src/components-lazy/ui/generic/Map.vue
@@ -27,7 +27,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
       <vl-layer-vector>
         <vl-source-cluster :distance="45">
-          <vl-source-vector :function="points || []"></vl-source-vector>
+          <vl-source-vector :features="points"></vl-source-vector>
         </vl-source-cluster>
         <vl-style-func :factory="markerStyleFunc" />
     </vl-layer-vector>
@@ -90,13 +90,18 @@ export default {
       )
     },
     points() {
-      return this.getCoordinatesOfCompanies(this.companiesWithValidLocationCoordinates) || [];
+      return this.getCoordinatesOfCompanies(this.companiesWithValidLocationCoordinates) || []
     },
     mapMarker() {
       return new OlIcon({
         color: this.primaryColor,
         crossOrigin: 'anonymous',
-        src: 'data:image/svg+xml;utf8,' + '<svg width="120" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path fill="white" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg>',
+        src: 'data:image/svg+xml;utf8,' + 
+          '<svg width="120" height="120" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">' + 
+            '<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->' + 
+            '<path fill="white" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>' + 
+            '<path fill="white" fill-opacity="0.1" d="M 106.66666,106.66666 H 277.33333 V 277.33333 H 106.66666 Z" />' + 
+          '</svg>',
         scale: 0.25,
       });
     },
