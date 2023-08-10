226651dbf3652a2ca36dd6dd53c35bb1d2c9277c
RaphaelSiller <raphael.siller123@gmail.com>
2023-08-10 11:21:39 +0200

    refactor: moved Header into it's own Component

--- src/pages/actors/_name.vue
+++ src/pages/actors/_name.vue
@@ -6,27 +6,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <template>
   <div class="actor-profile" style="height: 100vh; overflow: hidden">
-    <div class="header">
-      <div class="lang-selector">
-        <Select
-          :value="$i18n.locale"
-          :options="availableLanguages"
-          aspect="fill"
-          :white-contrast="false"
-          centered-text
-          @input="changeLanguage"
-        />
-      </div>
-      <div class="logos-ct">
-        <nuxt-link class="logo clickable" to="/">
-          <Icon name="logo" />
-        </nuxt-link>
-        <nuxt-link class="logo clickable" to="/">
-          <Icon name="logo-automotive" />
-        </nuxt-link>
-      </div>
-      <hr />
-    </div>
+    <HeaderNOI />
     <company-view
       :data="companyData"
       :visible="true"
@@ -86,60 +66,12 @@ export default {
         this.TITLE_END
       )
     },
-    availableLanguages() {
-      return [
-        {
-          value: 'it',
-          name: 'IT',
-        },
-        {
-          value: 'de',
-          name: 'DE',
-        },
-        {
-          value: 'en',
-          name: 'EN',
-        },
-      ]
-    },
-  },
-  mounted() {},
-  methods: {
-    changeLanguage(lang) {
-      if (typeof this.$i18n.setLocale !== 'undefined') {
-        this.$i18n.setLocale(lang)
-      } else {
-        this.$i18n.locale = lang
-      }
-    },
   },
 }
 </script>
 
 <style>
 .actor-profile {
-  & .header {
-    @apply fixed top-0 w-full bg-secondary z-20;
-
-    & .lang-selector {
-      @apply absolute top-6 right-4 w-14;
-    }
-    & .logos-ct {
-      height: 100px;
-
-      & .logo {
-        display: inline;
-
-        & svg {
-          @apply h-full;
-        }
-      }
-    }
-
-    & hr {
-      border-top: 1px solid black;
-    }
-  }
   & .right-column {
     @apply w-full;
 
