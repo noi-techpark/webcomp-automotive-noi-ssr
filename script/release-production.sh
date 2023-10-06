e75d55e395aae9847dbf1ea2494112829d9bfaf0
Simone Testa <sim.testa97@gmail.com>
2023-10-06 16:05:43 +0200

    ci: update deploy scripts

--- script/release-production.sh
+++ script/release-production.sh
@@ -7,18 +7,18 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-development_last_local_commit_sha=$(git log -1 --pretty=%H | cat) &&
+production_last_local_commit_sha=$(git log -1 --pretty=%H | cat) &&
 (git pull || git push --set-upstream origin main ) &&
-development_last_origin_commit_sha=$(git log -1 --pretty=%H | cat)
+production_last_origin_commit_sha=$(git log -1 --pretty=%H | cat)
 
-if [ "$development_last_local_commit_sha" != "$development_last_origin_commit_sha" ]; then 
+if [ "$production_last_local_commit_sha" != "$production_last_origin_commit_sha" ]; then 
   echo -e "\033[33mwarning\033[0m your local main commit is different from remote one"
   echo -e "\033[33mwarning\033[0m check the changes!"
   exit 1
 fi
 
-development_last_commit=$(git log -1 --pretty=%B | cat)
-if [[ "$development_last_commit" == "chore(release)"* ]]; then 
+production_last_commit=$(git log -1 --pretty=%B | cat)
+if [[ "$production_last_commit" == "chore(release)"* ]]; then 
   echo -e "\033[31merror\033[0m no changes were applied since last release"
   exit 1
 fi
